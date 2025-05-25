8c0ce4417365c9d4d40d671691419acfd035cf63
Binita-Sharma <binitasharma644@gmail.com>
2025-05-25 23:19:45 +0530

    added a project section and enhance the UI

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '../assets/images/grain.jpg';
 import StarIcon from '../assets/icons/star.svg';
+import SparkleIcon from '../assets/icons/sparkle.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
 
 export const HeroSection = () => {
   return (
 
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
       <div
         className="absolute inset-0 -z-30 opacity-5"
         style={{
@@ -32,6 +34,36 @@ export const HeroSection = () => {
       <HeroOrbit size={590} rotation={98}>
         <StarIcon className="size-12 text-emerald-300" />
       </HeroOrbit>
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className="size-10 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={720} rotation={85}>
+        <SparkleIcon className="size-3 rounded-full text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={520} rotation={-41}>
+        <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={650} rotation={-5}>
+        <SparkleIcon className="size-3 rounded-full text-emerald-300/20" />
+      </HeroOrbit>
+      </div>
+
 
       <div className="container">
         <div className="flex flex-col items-center relative">
@@ -59,15 +91,15 @@ export const HeroSection = () => {
         <div className="max-w-lg mx-auto mt-8">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide ">
             <span className="bg-gradient-to-r from-purple-300 via-indigo-300 to-blue-400 bg-clip-text text-transparent animate-gradient-shift bg-300%">
-              Building Exceptional User Experiences
+              Making Every Click Count with Intuitive Design
             </span>
           </h1>
 
           <p className="mt-6 text-center text-white/70 relative">
             <span className="relative" >
               I craft visually captivating designs into responsive, high-performing web applications that deliver real results.
-              Whether you're starting from scratch or leveling up an existing project,
-              I'm here to help bring your vision to life. Let’s create something exceptional—ready to chat about your next big idea?
+              Whether your starting from scratch or leveling up an existing project.
+              I am here to help bring your vision to life. Let’s create something exceptional—ready to chat about your next big idea?
             </span>
           </p>
         </div>
